c5a328c05c51ba8cc0ee07b635a9cab1c08d091e
ShkulovNikita <tyuiop_2@mail.ru>
2023-05-29 12:02:40 +0700

    Добавление выполнения первого задания.

---
+++ task1/header.php
@@ -0,0 +1,17 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title><? $APPLICATION->ShowTitle(); ?></title>
+    <? $APPLICATION->ShowHead();  ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="<?=SITE_TEMPLATE_PATH?>/images/favicon.604825ed.ico" type="image/x-icon">
+    <link href="<?=SITE_TEMPLATE_PATH?>/css/common.css" rel="stylesheet">
+</head>
+<body>
+<div id="panel">
+    <? $APPLICATION->ShowPanel(); ?> 
+</div>
+<header></header>
+
