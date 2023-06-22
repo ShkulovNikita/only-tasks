0d126d601774610b00b43207450f3e169456bf79
ShkulovNikita <tyuiop_2@mail.ru>
2023-06-22 13:44:36 +0700

    Создан рекурсивный алгоритм для получения разделов элемента инфоблока.

--- task5/dev.site/lib/Handlers/Iblock.php
+++ task5/dev.site/lib/Handlers/Iblock.php
@@ -11,13 +11,25 @@ class Iblock
     {
         \CModule::IncludeModule('iblock');
         /*
-         * Изменения в инфоблоках с кодом, содержащим "LOG",
-         * должны игнорироваться. 
+         * Срабатывания на лог-элементы (код "LOG" либо пустые код и имя) 
+         * должны игнорироваться.
          */
         $LOG_CODE = 'LOG';
-        if (stripos($arFields['CODE'], $LOG_CODE) !== false) 
+        if (
+            (($arFields['CODE'] === null) 
+            && ($arFields['NAME'] === null)) 
+            || (stripos($arFields['CODE'], $LOG_CODE) !== false)
+        ) {
             return;
-            
+        }
+        /*
+         * Получить разделы логируемого элемента инфоблока. 
+         */
+        $elPath = self::getPath(
+            $arFields['NAME'], 
+            $arFields['IBLOCK_ID'], 
+            $arFields['IBLOCK_SECTION']
+        );
         /*
          * Идентификатор раздела для лога
          */
@@ -52,7 +64,8 @@ class Iblock
                     'CODE' => $LOG_CODE,
                     'NAME' => $arFields['ID'],
                     'ACTIVE_FROM' => date('d.m.Y'),
-                    'ACTIVE' => 'Y'
+                    'ACTIVE' => 'Y',
+                    'PREVIEW_TEXT' => $elPath
                 )
             );
         /*
@@ -63,7 +76,8 @@ class Iblock
             $result = $log->Update(
                 $logIblockID,
                 Array(
-                    'ACTIVE_FROM' => date('d.m.Y')
+                    'ACTIVE_FROM' => date('d.m.Y'),
+                    'PREVIEW_TEXT' => $elPath
                 )
             );
         }
@@ -81,13 +95,13 @@ class Iblock
          * Найти раздел 
          */
         $arSections = \CIBlockSection::GetList(
-            Array('SORT' => 'ASC'),
-            Array(
+            [],
+            [
                 'NAME' => $elName,
                 'CODE' => $elCode
-            ),
+            ],
             false,
-            Array('ID')
+            ['ID']
         );
 
         /*
@@ -110,15 +124,15 @@ class Iblock
     private static function getLogElement($elID, $elCode, $sectionID)
     {
         $arElements = \CIBlockElement::GetList(
-            Array('SORT' => 'ASC'),
-            Array(
+            [],
+            [
                 'NAME' => $elID,
                 'CODE' => $elCode,
                 'SECTION_ID' => $sectionID
-            ),
+            ],
             false,
             false,
-            Array('ID')
+            ['ID']
         );
 
         while ($arEl = $arElements->GetNext()) {
@@ -126,6 +140,87 @@ class Iblock
         }
 
         return false;
+    }
+
+    /**
+     * Получение пути до элемента инфоблока в формате
+     * "Имя инфоблока -> Имя раздела(от родителя к ребенку)... -> Имя элемента".
+     * @param string $elName Имя элемента инфоблока.
+     * @param int $iblockID Идентификатор инфоблока элемента.
+     * @param int $lowerSectionID Идентификатор непосредственного раздела элемента.
+     * @return string Путь до элемента инфоблока в указанном формате.
+     */
+    private static function getPath($elName, $iblockID, $lowerSectionID)
+    {
+        /*
+         * Добавить в конец пути имя элемента инфоблока. 
+         */
+        $path = $elName;
+        /*
+         * Если у элемента есть родительский раздел, то найти его родительские разделы. 
+         */
+        if (isset($lowerSectionID)) {
+            self::getSections($path, $lowerSectionID[0]);
+        }
+        /*
+         * Получить имя инфоблока.
+         */
+        $iblockName = self::getIblockName($iblockID);
+        /*
+         * Добавить в начало название инфоблока. 
+         */
+        if ($iblockName !== false) {
+            $path = $iblockName . ' -> ' . $path;
+        }
+
+        return $path;
+    }
+
+    /**
+     * Рекурсивная функция поиска родительских разделов элемента инфоблока.
+     * @param string $path Путь с родительскими разделами инфоблока.
+     * @param int $sectionID Идентификатор очередного родительского раздела.
+     */
+    private static function getSections(&$path, $sectionID)
+    {
+        /*
+         * Получить данные о текущем разделе: имя и старший раздел.
+         */
+        $arSection = \CIBlockSection::GetList(
+            [],
+            ['ID' => $sectionID],
+            false,
+            ['ID', 'IBLOCK_SECTION_ID', 'NAME']
+        );
+        $sectionInfo = $arSection->getNext();
+        /*
+         * Добавить имя раздела к пути /
+         */
+        $path = $sectionInfo['NAME'] . ' -> ' . $path;
+        /*
+         * Если есть родительский раздел, то вызвать функцию ещё раз,
+         * иначе вернуть полученный путь. 
+         */
+        if (isset($sectionInfo['IBLOCK_SECTION_ID'])) {
+            self::getSections($path, $sectionInfo['IBLOCK_SECTION_ID']);
+        } else {
+            return;
+        }
+    }
+
+    /**
+     * Получение имени инфоблока, которому принадлежит указанный элемент.
+     * @param int $elID Идентификатор элемента инфоблока.
+     * @return string|bool Имя информационного блока либо false.
+     */
+    private static function getIblockName($elID)
+    {
+        $iblockRes = \CIBlock::GetByID($elID);
+        if ($iblock = $iblockRes->GetNext()) {
+            return $iblock['NAME'];
+        } else {
+            return false;
+        }
     }
 
     function OnBeforeIBlockElementAddHandler(&$arFields)
