01fe07d0b19106692967767cf3ec622932613b3c
ShkulovNikita <tyuiop_2@mail.ru>
2023-06-27 12:17:08 +0700

    Реализовано подключение классов через автозагрузчик Composer'а.

--- task5/php_interface/init.php
+++ task5/php_interface/init.php
@@ -1,7 +1,17 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/vendor/autoload.php');
+if (file_exists($_SERVER["DOCUMENT_ROOT"] . "/local/php_interface/autoload.php")) {
+    require_once($_SERVER["DOCUMENT_ROOT"] . "/local/php_interface/autoload.php");
+}
 
 $eventManager = \Bitrix\Main\EventManager::getInstance();
+$eventManager->AddEventHandler(
+    "iblock",
+    "OnAfterIBlockElementAdd",
+    [
+        "Only\\Site\\Handlers\\Iblock",
+		'addLog'
+    ]
+);
 $eventManager->AddEventHandler(
 	"iblock",
 	"OnAfterIBlockElementUpdate",
--- task5/php_interface/vendor/autoload.php
+++
@@ -1,5 +0,0 @@
-<?
-Bitrix\Main\Loader::registerAutoLoadClasses(null, [
-    'Only\Site\Handlers\Iblock' => '/local/modules/dev.site/lib/Handlers/Iblock.php',
-    'Only\Site\Agents\Iblock' => '/local/modules/dev.site/lib/Agents/Iblock.php'
-]);
