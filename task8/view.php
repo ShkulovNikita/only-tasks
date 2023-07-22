a5668ff9c2d04bc24e64a789e586e6c8dee3394e
ShkulovNikita <tyuiop_2@mail.ru>
2023-07-22 22:04:46 +0700

    Оформление страницы просмотра информации о файле.

--- task8/view.php
+++ task8/view.php
@@ -14,13 +14,15 @@ echo HtmlHelper::showProlog('Просмотр файла');
 <?php
 $file = FileController::view();
 ?>
-
 <div class="container-fluid">
     <?=HtmlHelper::showHeader();?>
-    <div class="row">
-        <?php 
-        if ($file !== '') {
-            ?>
+    <div class="row wide-errors">
+        <?=HtmlHelper::showMessage();?>
+    </div>
+    <?php 
+    if ($file !== '') {
+    ?>
+        <div class="row main-content view-content">
             <!-- Изображение для файла. -->
             <div class="col-6" class="file-image">
                 <?php
@@ -42,44 +44,51 @@ $file = FileController::view();
             </div>
             <!-- Вывести информацию о файле. -->
             <div class="col-6" class="file-menu">
-                <h3><?=$file['name']?></h3>
-                <h5>Информация о файле:</h5>
-                <p>Дата создания: <?=FileHelper::getFileDate($file->created)?></p>
-                <p>Последнее изменение: <?=FileHelper::getFileDate($file->modified)?></p>
-                <p>Размер: <?=FileHelper::getFileSize($file->size)?></p>
-                <p>Тип файла: <?=$file->mime_type?></p>
-                <?php
-                $properties = $file->getProperties();
-                if ($properties) {
-                    ?>
-                    <h5>Метаинформация:</h5>
+                <h3 class="view-content__file-name"><?=$file['name']?></h3>
+                <div class="view-content__buttons-menu">
+                    <div class="view-content__edit-button">
+                        <a href="edit.php?name=<?=$file['name']?>" class="btn btn-secondary button_yellow">Редактировать</a>
+                    </div>
+                    <div class="view-content__download-button">
+                        <form action="download.php" method="POST">
+                            <input type="hidden" name="download" value="<?=$file['name']?>">
+                            <input type="submit" class="btn btn-secondary button_yellow" value="Скачать">
+                        </form>
+                    </div>
+                    <div class="view-content__delete-button">
+                        <form action="delete.php" method="POST">
+                            <input type="hidden" name="fileForDelete" value="<?=$file['name']?>">
+                            <input type="submit" class="btn btn-danger" value="Удалить">
+                        </form>
+                    </div>
+                </div>
+                <div class="view-content__file-info">
+                    <h5>Информация о файле:</h5>
+                    <p>Дата создания: <?=FileHelper::getFileDate($file->created)?></p>
+                    <p>Последнее изменение: <?=FileHelper::getFileDate($file->modified)?></p>
+                    <p>Размер: <?=FileHelper::getFileSize($file->size)?></p>
+                    <p>Тип файла: <?=$file->mime_type?></p>
                     <?php
-                    foreach ($properties as $propertyKey => $propertyValue) {
+                    $properties = $file->getProperties();
+                    if ($properties) {
                         ?>
-                        <p><?=$propertyKey?>: <?=$propertyValue?></p>
+                        <h5>Метаинформация:</h5>
                         <?php
+                        foreach ($properties as $propertyKey => $propertyValue) {
+                            ?>
+                            <p><?=$propertyKey?>: <?=$propertyValue?></p>
+                            <?php
+                        }
                     }
-                }
-                ?>
-                <a href="<?=$file['docviewer']?>" target="_blank" class="btn btn-secondary yandex-button">Открыть в Яндекс.Диске</a>
-                <form action="download.php" method="POST">
-                    <input type="hidden" name="download" value="<?=$file['name']?>">
-                    <input type="submit" class="btn btn-info" value="Скачать">
-                </form>
-                <form action="delete.php" method="POST">
-                    <input type="hidden" name="fileForDelete" value="<?=$file['name']?>">
-                    <input type="submit" class="btn btn-danger" value="Удалить">
-                </form>
-                <a href="edit.php?name=<?=$file['name']?>" class="btn btn-primary">Редактировать</a>
-                <a href="index.php" class="btn btn-primary">Назад</a>
+                    ?>
+                    <a href="<?=$file['docviewer']?>" target="_blank" class="btn btn-secondary yandex-button">Открыть в Яндекс.Диске</a>
+                </div>
             </div>
-            <?php
-        }
-        ?>
-    </div>
-    <div class="row">
-        <?=HtmlHelper::showFooter();?>
-    </div>
+        </div>
+        <?php
+    }
+    ?>
+    <?=HtmlHelper::showFooter();?>
 </div>
 </body>
 </html>
