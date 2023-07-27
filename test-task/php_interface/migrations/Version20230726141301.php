422d6caa9e79a8d979fce9dc76317e604064b9b0
ShkulovNikita <tyuiop_2@mail.ru>
2023-07-27 09:46:08 +0700

    Реализовано добавление инфоблока водителей через миграцию.

--- test-task/php_interface/migrations/Version20230726141301.php
+++ test-task/php_interface/migrations/Version20230726141301.php
@@ -17,14 +17,18 @@ class Version20230726141301 extends Version
     public function up()
     {
         $helper = $this->getHelperManager();
-        /**
+        /*
          * Добавление хайлоад-блока для марок автомобилей.
          */
         $carBrandsHlBlockId = $this->carBrandUp($helper);
-        /**
+        /*
          * Добавить раздел инфоблоков.
          */
-        $this->iblockSectionUp($this->iblockSectionID, $helper);
+        $this->iblockSectionUp($helper);
+        /*
+         * Добавить инфоблок водителей.
+         */
+        $driversIblockId = $this->driversUp($helper);
     }
 
     /**
@@ -33,10 +37,14 @@ class Version20230726141301 extends Version
     public function down()
     {
         $helper = $this->getHelperManager();
-        /**
+        /*
          * Удалить хайлоад-блок марок автомобилей.
          */
         $this->carBrandDown($helper);
+        /*
+         * Удалить инфоблок водителей.
+         */
+        $this->driversDown($helper);
     }
 
     /**
@@ -82,13 +90,12 @@ class Version20230726141301 extends Version
 
     /**
      * Добавить раздел для добавляемых инфоблоков.
-     * @param int $iblockSectionID Идентификатор раздела инфоблоков.
      * @param HelperManager $helper Менеджер для выполнения действий миграции.
      */
-    private function iblockSectionUp($iblockSectionID, $helper)
+    private function iblockSectionUp($helper)
     {
         $helper->Iblock()->saveIblockType([
-            'ID' => $iblockSectionID,
+            'ID' => $this->iblockSectionID,
             'LANG' => [
                 'en' => [
                     'NAME' => 'Job cars',
@@ -102,6 +109,73 @@ class Version20230726141301 extends Version
                 ],
             ],
         ]);
+    }
+
+    /**
+     * Добавить инфоблок водителей служебных автомобилей.
+     * @param HelperManager $helper Менеджер для выполнения действий миграции.
+     * @return int Идентификатор созданного инфоблока.
+     */
+    private function driversUp($helper)
+    {
+        $driversIblockId = $helper->Iblock()->saveIblock([
+            'NAME' => 'Водители',
+            'CODE' => 'job_cars_drivers',
+            'LID' => ['s1'],
+            'IBLOCK_TYPE_ID' => $this->iblockSectionID,
+            'LIST_PAGE_URL' => '',
+            'DETAIL_PAGE_URL' => '#SITE_DIR#/news/#ELEMENT_ID#',
+        ]);
+        
+        $helper->Iblock()->saveIblockFields($driversIblockId, [
+            'CODE' => [
+                'DEFAULT_VALUE' => [
+                    'TRANSLITERATION' => 'Y',
+                    'UNIQUE' => 'Y',
+                ],
+            ],
+        ]);
+
+        $arProps = [
+            [
+                'NAME' => 'Фамилия',
+                'CODE' => 'SURNAME',
+                'PROPERTY_TYPE' => 'S',
+                'IS_REQUIRED' => 'Y'
+            ],
+            [
+                'NAME' => 'Имя',
+                'CODE' => 'NAME',
+                'PROPERTY_TYPE' => 'S',
+                'IS_REQUIRED' => 'Y'
+            ],
+            [
+                'NAME' => 'Отчество',
+                'CODE' => 'PATRONYMIC',
+                'PROPERTY_TYPE' => 'S'
+            ],
+            [
+                'NAME' => 'Номер телефона',
+                'CODE' => 'PHONE',
+                'PROPERTY_TYPE' => 'S'
+            ]
+        ];
+        if ($driversIblockId) {
+            foreach ($arProps as $arProp) {
+                $helper->Iblock()->addPropertyIfNotExists($driversIblockId, $arProp);
+            }
+
+            $helper->AdminIblock()->buildElementForm($driversIblockId, [
+                'Личные данные' => [
+                    'PROPERTY_SURNAME',
+                    'PROPERTY_NAME',
+                    'PROPERTY_PATRONYMIC',
+                    'PROPERTY_PHONE'
+                ]
+            ]);
+        }
+
+        return $driversIblockId;
     }
 
     /**
@@ -120,5 +194,14 @@ class Version20230726141301 extends Version
             ]
         );
         $helper->Hlblock()->deleteHlblock($carBrandsHlBlockId);
+    }
+
+    /**
+     * Удалить инфоблок водителей.
+     * @param HelperManager $helper Менеджер для выполнения действий миграции.
+     */
+    private function driversDown($helper)
+    {
+        $result = $helper->Iblock()->deleteIblockIfExists('job_cars_drivers');
     }
 }
